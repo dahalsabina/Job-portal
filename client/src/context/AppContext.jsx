025d85040658e9307f2ef838ba2ed2f0bb2d1598
dahalSabina <dahasa01@luther.edu>
2025-01-31 16:59:00 -0600

    added fronted functionalities for filtering jobs based on location, category and searchFilters

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,37 @@
-import { createContext } from "react";
+import { createContext,useEffect,useState } from "react";
+import { jobsData } from "../assets/assets";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props)=>{
+
+    const [searchFilter,setSearchFilter]=useState({
+        title:'',
+        location:''
+
+    })
+    const [isSearched,setIsSearched] = useState(false)
+    const [jobs,setJobs] =useState([])
+
+    //Function to fetch job data
+    const fetchJobs = async()=>{
+        setJobs(jobsData)
+
+    }
+
+    useEffect(()=>{
+        fetchJobs()
+
+    },[])
+
+
+
+
+
     const value = {
+        setSearchFilter, searchFilter,
+        isSearched,setIsSearched,
+        jobs,setJobs
 
     }
    
